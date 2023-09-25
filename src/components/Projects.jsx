fcbbde52643e2865329364f1316a6dcc263f4a63
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-09-25 17:15:45 +0530

    edit images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,36 +49,56 @@ const Projects = () => {
       src: k1,
       githubLink: "https://github.com/GroupCS31-UCSC/Koratuwa",
       demoImages: [k1, k2, k3, k4, k5, k6, k7],
+      description:
+        "Koratuwa is Full Stack Web Application developed to manage internal and external operations of a dairy farm located at Horana, named 'Koratuwa'. It is developed as our second year Group Project in university.\n I developed Developed Admin portal and Milk Collection Officer role (both frontend and backend), notification handling, user authentication, session management, and phpMailer-email sending.",
+      technologies: "HTML, CSS, Javascript, PHP, jQuery",
     },
     {
       id: 2,
       src: e1,
       githubLink: "https://github.com/Group45-UCSC/EduGo",
       demoImages: [e1, e2, e3, e4, e5],
+      description:
+        "Edugo is Full Stack Web Application developed to handle school rides of the school children. It is developed as our third year Group Project in university. \n I developed Driver role in web application (both frontend and backend) and google map handling part. Working with a team of 6 members and holding team leadership.",
+      technologies:
+        "ReactJs, ExpressJs, NodeJs, TailwindCSS, PostgreSQL, javascript",
     },
     {
       id: 3,
-      src: s1,
-      githubLink: "https://github.com/ApsaraLiyanage99/fullstack_springApp",
-      demoImages: [s1, s2],
-    },
-    {
-      id: 4,
       src: m1,
       githubLink: "https://github.com/ApsaraLiyanage99/mern_medApp",
       demoImages: [m1, m2, m3, m4, m5, m6],
+      description:
+        "Medi Care is Frontend Application. I developed landing page of a medical care center, which is implemented for the improvement of my frontend development skills.",
+      technologies: "ReactJs, Bootstrap",
     },
+
     {
-      id: 5,
+      id: 4,
       src: t1,
       githubLink: "https://github.com/ApsaraLiyanage99/pern_todoApp",
       demoImages: [t1, t2],
+      description:
+        "Todo is a simple Full Stack Web Application, I built to practice the pern stack development for the learning objective. ",
+      technologies: "ReactJs, NodeJs, ExpressJs, PostgreSQL",
     },
     {
-      id: 6,
+      id: 5,
       src: c1,
       githubLink: "https://github.com/ApsaraLiyanage99/kotlin_calculatorApp",
       demoImages: [c1, c2, c3],
+      description:
+        "This is a Mobile Application, I developed to perform basic calculator operations using Android Studio. ",
+      technologies: "Kotlin",
+    },
+    {
+      id: 6,
+      src: s1,
+      githubLink: "https://github.com/ApsaraLiyanage99/fullstack_springApp",
+      demoImages: [s1, s2],
+      description:
+        "Simple Full Stack Web Application, I developed to perform CRUD operations in an Employee management system, for the learning perspective of Spring Boot framework and practice testing using postman. ",
+      technologies: "Java, Spring Boot, ReactJs, MySQL, Axios",
     },
   ];
 
@@ -93,6 +113,20 @@ const Projects = () => {
   const closeModal = () => {
     setSelectedProject(null);
     setIsModalOpen(false);
+  };
+
+  // --------------------------------------------------------------
+  const [isModalOpen_data, setIsModalOpen_data] = useState(false);
+  const [selectedProject_data, setSelectedProject_data] = useState(null);
+
+  const openModal_data = (project) => {
+    setSelectedProject_data(project);
+    setIsModalOpen_data(true);
+  };
+
+  const closeModal_data = () => {
+    setSelectedProject_data(null);
+    setIsModalOpen_data(false);
   };
 
   return (
@@ -129,6 +163,12 @@ const Projects = () => {
                   Demo
                 </button>
                 <button
+                  onClick={() => openModal_data(project)}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Details
+                </button>
+                <button
                   onClick={() => window.open(project.githubLink, "_blank")}
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                 >
@@ -152,7 +192,7 @@ const Projects = () => {
         {/* <div> */}
         <div className="bg-white p-0 px-60 rounded-lg ">
           <div className="fixed top-0 left-0 w-screen  bg-stone-900/75 flex justify-center items-center  h-screen bg-gradient-to-b from-opacity-70 to-opacity-30">
-            <div className="w-1/3  rounded-lg bg-white shadow-md flex flex-col p-5 ">
+            <div className="w-2/4  rounded-lg bg-white shadow-md flex flex-col p-5 ">
               <div className="flex justify-end">
                 <button
                   className="text-2xl cursor-pointer "
@@ -163,7 +203,6 @@ const Projects = () => {
                   X
                 </button>
               </div>
-
               {selectedProject && (
                 <>
                   <Carousel>
@@ -173,10 +212,59 @@ const Projects = () => {
                       </div>
                     ))}
                   </Carousel>
-                  <button className="close-button" onClick={closeModal}>
+                  <button
+                    className="bg-gray-700 text-white py-2 px-4 rounded-md mt-6 hover:bg-gray-400 transition duration-300"
+                    onClick={closeModal}
+                  >
                     Close
                   </button>
                 </>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+      </Modal>
+
+      {/* details modal */}
+      <Modal
+        isOpen={isModalOpen_data}
+        onRequestClose={closeModal_data}
+        contentLabel="Project Details"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        {/* <div> */}
+        <div className="bg-white p-0 px-60 rounded-lg ">
+          <div className="fixed top-0 left-0 w-screen  bg-stone-900/75 flex justify-center items-center  h-screen bg-gradient-to-b from-opacity-70 to-opacity-30">
+            <div className="w-2/4  rounded-lg bg-white shadow-md flex flex-col p-5 ">
+              <div className="flex justify-end">
+                <button
+                  className="text-2xl cursor-pointer "
+                  onClick={() => {
+                    setIsModalOpen_data(false);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+
+              {selectedProject_data && (
+                <div className="text-center">
+                  <h2 className="text-1xl mb-4">
+                    {selectedProject_data.description}
+                  </h2>
+                  <p className="text-gray-600 font-semibold mb-4">
+                    <span className="font-bold text-orange-900">Technologies:</span>{" "}
+                    {selectedProject_data.technologies}
+                  </p>
+                  <button
+                    className="bg-gray-700 text-white py-2 px-4 rounded-md mt-6 hover:bg-gray-400 transition duration-300"
+                    onClick={closeModal_data}
+                  >
+                    Close
+                  </button>
+                </div>
               )}
             </div>
           </div>
